c5a663d0a3242440db415338735720b190ed0fa2
Deezinn <developer.andrelz@gmail.com>
2025-09-14 21:09:33 -0300

    feat: new generate doc

--- src/utils/generate_doc_fields.py
+++ src/utils/generate_doc_fields.py
@@ -1,15 +1,23 @@
 from src.constant import DESCRICAO
+import os 
+
 
 def denerateDocFields(fields):
-    output="src/docs/fields.txt"
+    output = 'src/docs/fields.txt'
     
-    if not fields:
-        print("Não consegui achar os campos")
-        return
-    with open(output, "w", encoding="utf-8") as f:
-        f.write("Campos e seu significados com seus tipos. \n\n\n")
-        for field in fields:
-            fid = field['id']
-            ftype = field['type']
-            desc = DESCRICAO.get(fid, "Sem descrição")
-            f.write(f"{fid} ({ftype}): {desc} \n \n")
+    try:
+        os.makedirs(output[:8], exist_ok=True)
+        print("Diretório criado com sucesso")
+    except OSError as error:
+        print("Diretório não foi criado com sucesso.", error)
+    else:
+        if not fields:
+            print("Não consegui achar os campos")
+            return
+        with open(output, "w", encoding="utf-8") as f:
+            f.write("Campos e seu significados com seus tipos. \n\n\n")
+            for field in fields:
+                fid = field['id']
+                ftype = field['type']
+                desc = DESCRICAO.get(fid, "Sem descrição")
+                f.write(f"{fid} ({ftype}): {desc} \n \n")
