f63a97f21b5dff65dc2a83dacd8e95b39d5b1f86
Deezinn <developer.andrelz@gmail.com>
2025-09-17 23:37:42 -0300

    feat: resolve path

--- src/etl/load.py
+++ src/etl/load.py
@@ -4,11 +4,11 @@ import pandas as pd
 class EmpresaLoad(EmpresaLoadInterface):
     def saveRawCsv(self, campos, dados):
         import os
-        if not os.path.exists('csv/raw/'):
-            os.makedirs('csv/raw/')
+        if not os.path.exists('data/csv/raw/'):
+            os.makedirs('data/csv/raw/')
         else:
-            pd.DataFrame(dados).to_csv('csv/raw/Fields.csv')  
-            pd.DataFrame(campos).to_csv('csv/raw/Empresas.csv')
+            pd.DataFrame(campos).to_csv('data/csv/raw/Fields.csv')  
+            pd.DataFrame(dados).to_csv('data/csv/raw/Empresas.csv')
         return pd.DataFrame(dados), pd.DataFrame(campos)
     
     def saveProcessCsv(self, dados):
