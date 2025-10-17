59b73afcc8a3a521366e8929b6aa9842d8048c4a
Deezinn <developer.andrelz@gmail.com>
2025-10-16 23:33:51 -0300

    feat: new interface

--- src/etl/transforms/transform_inativa.py
+++ src/etl/transforms/transform_inativa.py
@@ -1,83 +1,79 @@
-from src.interface import EmpresaAtivaTransformInterface
+from src.interface import EmpresaInativaTransformInterface
 from io import StringIO
 import pandas as pd
 
-class EmpresaInativaTransform(EmpresaAtivaTransformInterface):
+class EmpresaInativaTransform(EmpresaInativaTransformInterface):
     def __init__(self):
-        self.__raw_inativas = None
-        self.__df = None
-        self.__text_default = 'Não informado'
+        self._raw_inativas = None
+        self._df = None
+        self._text_default = 'Não informado'
         
     def transform(self, inativosCsv):
-        self.__raw_inativas = inativosCsv
-        self.__transform_to_dataframe()
-        self.__rename_columns()
-        self.__transform_cnpj_column()
-        self.__transform_razao_social_column()
-        self.__transform_nome_fantasia_column()
-        self.__transform_cod_logradouro_column()
-        self.__transform_nome_logradouro()
-        self.__transform_numero_residencia_column()
-        self.__transform__numero_lote_column()
-        self.__transform_cod_bairro_column()
-        self.__transform_nome_bairro_column()
-        self.__transform_situacao_empresa_column()
+        """
+        Método orquestrador que aplica todas as transformações
+        """
+        self._raw_inativas = inativosCsv
+        self._transform_to_dataframe()
+        self._rename_columns()
+        self._transform_cnpj_column()
+        self._transform_razao_social_column()
+        self._transform_nome_fantasia_column()
+        self._transform_cod_logradouro_column()
+        self._transform_nome_logradouro_column()
+        self._transform_numero_residencia_column()
+        self._transform_numero_lote_column()
+        self._transform_cod_bairro_column()
+        self._transform_nome_bairro_column()
+        self._transform_situacao_empresa_column()
         
-    def __transform_to_dataframe(self):
-        self.__df = pd.read_csv(
-            StringIO(self.__raw_inativas),
+    def _transform_to_dataframe(self):
+        """Converte CSV para DataFrame"""
+        self._df = pd.read_csv(
+            StringIO(self._raw_inativas),
             sep=';',
             dtype=str,
-            encoding='utf-8-sig' 
+            encoding='utf-8-sig',
+            low_memory=False
         )
-        
-    def __rename_columns(self):
-        self.__df.rename({'ï»¿cnpj': 'cnpj'}, axis=1, inplace=True)
-        return self.__df
-        
-    def __transform_cnpj_column(self):
-        cnpj_invalidos = self.__df['cnpj'].str.contains(r'[^0-9]', na=False)
-        self.__df = self.__df[~cnpj_invalidos]
-        return self.__df
 
-    def __transform_razao_social_column(self):
-        self.__df['razao_social'] = self.__df['razao_social'].replace({'.': self.__text_default})
-        self.__df['razao_social'] = self.__df['razao_social'].fillna(self.__text_default)
-        self.__df['razao_social'] = self.__df['razao_social'].str.title()
-        return self.__df
-    
-    def __transform_nome_fantasia_column(self):
-        self.__df['nome_fantasia'] = self.__df['nome_fantasia'].replace({'.': self.__text_default})
-        self.__df['nome_fantasia'] = self.__df['nome_fantasia'].fillna(self.__text_default)
-        self.__df['nome_fantasia'] = self.__df['nome_fantasia'].str.title()
-        return self.__df
-    
-    def __transform_cod_logradouro_column(self):
-        self.__df['cod_logradouro'] = self.__df['cod_logradouro'].fillna(self.__text_default)
-        return self.__df
-    
-    def __transform_nome_logradouro(self):
-        self.__df['nome_logradouro'] = self.__df['nome_logradouro'].fillna(self.__text_default)
-        return self.__df
-    
-    def __transform_numero_residencia_column(self):
-        self.__df['numero_residencia'] = self.__df['numero_residencia'].fillna(self.__text_default)
-        return self.__df
-    
-    def __transform__numero_lote_column(self):
-        self.__df['numero_lote'] = self.__df['numero_lote'].fillna(self.__text_default)
-        return self.__df
-    
-    def __transform_cod_bairro_column(self):
-        self.__df['cod_bairro'] = self.__df['cod_bairro'].fillna(self.__text_default)
-        return self.__df
-    
-    def __transform_nome_bairro_column(self):
-        self.__df['nome_bairro'] = self.__df['nome_bairro'].fillna(self.__text_default)
-        self.__df['nome_bairro'] = self.__df['nome_bairro'].str.title()
-        return self.__df
-    
-    def __transform_situacao_empresa_column(self):
-        self.__df['situacao_empresa'] = self.__df['situacao_empresa'].fillna(self.__text_default)
-        self.__df['situacao_empresa'] = self.__df['situacao_empresa'].str.title()
-        return self.__df
+    def _rename_columns(self):
+        """Renomeia colunas problemáticas"""
+        self._df.rename({'ï»¿cnpj': 'cnpj'}, axis=1, inplace=True)
+
+    def _transform_cnpj_column(self):
+        """Limpa CNPJs, mantendo apenas números"""
+        self._df['cnpj'] = self._df['cnpj'].str.replace(r'\D', '', regex=True)
+
+    def _transform_razao_social_column(self):
+        """Normaliza 'razao_social' e preenche valores ausentes"""
+        self._df['razao_social'] = self._df['razao_social'].replace({'.': self._text_default})
+        self._df['razao_social'] = self._df['razao_social'].fillna(self._text_default)
+        self._df['razao_social'] = self._df['razao_social'].str.title()
+
+    def _transform_nome_fantasia_column(self):
+        self._df['nome_fantasia'] = self._df['nome_fantasia'].replace({'.': self._text_default})
+        self._df['nome_fantasia'] = self._df['nome_fantasia'].fillna(self._text_default)
+        self._df['nome_fantasia'] = self._df['nome_fantasia'].str.title()
+
+    def _transform_cod_logradouro_column(self):
+        self._df['cod_logradouro'] = self._df['cod_logradouro'].fillna(self._text_default)
+
+    def _transform_nome_logradouro_column(self):
+        self._df['nome_logradouro'] = self._df['nome_logradouro'].fillna(self._text_default)
+
+    def _transform_numero_residencia_column(self):
+        self._df['numero_residencia'] = self._df['numero_residencia'].fillna(self._text_default)
+
+    def _transform_numero_lote_column(self):
+        self._df['numero_lote'] = self._df['numero_lote'].fillna(self._text_default)
+
+    def _transform_cod_bairro_column(self):
+        self._df['cod_bairro'] = self._df['cod_bairro'].fillna(self._text_default)
+
+    def _transform_nome_bairro_column(self):
+        self._df['nome_bairro'] = self._df['nome_bairro'].fillna(self._text_default)
+        self._df['nome_bairro'] = self._df['nome_bairro'].str.title()
+
+    def _transform_situacao_empresa_column(self):
+        self._df['situacao_empresa'] = self._df['situacao_empresa'].fillna(self._text_default)
+        self._df['situacao_empresa'] = self._df['situacao_empresa'].str.title()
--- src/interface/transforms/transform_interface_inativa.py
+++ src/interface/transforms/transform_interface_inativa.py
@@ -2,19 +2,71 @@ from abc import ABC, abstractmethod
 
 class EmpresaInativaTransformInterface(ABC):
     """
-    Interface para transformação de dados da API de empresas.
+    Interface para transformação de dados da API de empresas inativas.
+    Define métodos obrigatórios que qualquer classe concreta deve implementar.
     """
-    
+
     @abstractmethod
     def transform(self, inativosCsv):
         """
-        Processar os dados (atuará como orquestrador)
+        Método principal que orquestra toda a transformação dos dados.
+        Recebe o CSV de empresas inativas.
         """
         pass
-    
+
     @abstractmethod
-    def __transform_to_dataframe():
+    def _rename_columns(self):
         """
-        Transformar em dataframe os dados de empresas Inativas
+        Renomeia colunas do DataFrame conforme padrão definido.
         """
+        pass
+
+    @abstractmethod
+    def _transform_cnpj_column(self):
+        """Transforma a coluna CNPJ (padroniza, remove inválidos)."""
+        pass
+
+    @abstractmethod
+    def _transform_razao_social_column(self):
+        """Transforma a coluna 'razao_social' (normaliza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_nome_fantasia_column(self):
+        """Transforma a coluna 'nome_fantasia' (normaliza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_cod_logradouro_column(self):
+        """Transforma a coluna 'cod_logradouro' (padroniza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_nome_logradouro_column(self):
+        """Transforma a coluna 'nome_logradouro' (normaliza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_numero_residencia_column(self):
+        """Transforma a coluna 'numero_residencia' (padroniza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_numero_lote_column(self):
+        """Transforma a coluna 'numero_lote' (padroniza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_cod_bairro_column(self):
+        """Transforma a coluna 'cod_bairro' (padroniza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_nome_bairro_column(self):
+        """Transforma a coluna 'nome_bairro' (normaliza e preenche nulos)."""
+        pass
+
+    @abstractmethod
+    def _transform_situacao_empresa_column(self):
+        """Transforma a coluna 'situacao_empresa' (padroniza e preenche nulos)."""
         pass
