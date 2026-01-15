a1a8fa38d4141a82030b0fa22f4c05d3fa2fb3c5
Deezinn <developer.andrelz@gmail.com>
2026-01-15 18:34:48 -0300

    feat: create constant to setup project

---
+++ src/core/constants/urls.py
@@ -0,0 +1,9 @@
+"""
+    Fonte de dados: http://dados.recife.pe.gov.br/dataset/empresas-da-cidade-do-recife
+    Última atualização: 09-01-2026 
+"""
+
+urls = {
+    'empresa_ativa': 'http://dados.recife.pe.gov.br/dataset/eb9b8a72-6e51-4da2-bc2b-9d83e1f198b9/resource/87fc9349-312c-4dcb-a311-1c97365bd9f5/download/empresasativender.csv',
+    'empresa_inativa': 'http://dados.recife.pe.gov.br/dataset/eb9b8a72-6e51-4da2-bc2b-9d83e1f198b9/resource/edc40277-c83f-4fa3-bfa5-1b85881921d8/download/empresasinativender.csv'
+}
